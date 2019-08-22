0e2cbff6388b24660fc6579a967c9d3a671f2873
Allan Passos <allansuru@hotmail.com>
2019-08-22 17:47:47 -0300

    fix: adjust card number

--- src/app/shared/credit-card/credit-card.component.ts
+++ src/app/shared/credit-card/credit-card.component.ts
@@ -18,7 +18,7 @@ export class CreditCardComponent implements OnInit, OnDestroy {
   public creditCard: CreditCard = {
     name: "NOME DO TITULAR",
     cvv: 0,
-    numCard: "**** **** **** *",
+    numCard: "**** **** **** ****",
     validate: "00/00"
   };
 
